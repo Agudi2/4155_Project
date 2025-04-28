d69b07281ff195797b534f6ffec3545da6892ecc
Sborrello <samborrello3@gmail.com>
2025-04-27 21:03:24 -0400

    adds face ID login

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from db.mongo import init_db
 from routes.predict import predict_blueprint
 from routes.register import register_blueprint
 from routes.analyze import analyze_blueprint
+from routes.login_face import login_face_blueprint
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -15,6 +16,7 @@ init_db(app)
 app.register_blueprint(predict_blueprint, url_prefix='/api')
 app.register_blueprint(register_blueprint, url_prefix='/api')
 app.register_blueprint(analyze_blueprint, url_prefix='/api')
+app.register_blueprint(login_face_blueprint, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
