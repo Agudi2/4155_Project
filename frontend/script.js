87a1d3701a31678ddf67dee3af4e8b92cffe4a74
Agudi2 <113403295+Agudi2@users.noreply.github.com>
2025-04-30 21:41:27 -0400

    connecting backend with frontend

--- frontend/script.js
+++ frontend/script.js
@@ -85,7 +85,7 @@ async function captureAndPredict() {
 
     try {
         statusDisplay.textContent = 'Sending for prediction...';
-        const response = await fetch('http://127.0.0.1:5000/api/predict', {
+        const response = await fetch('https://four155-project.onrender.com/api/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ image: imageData })
